26203b819cc551f28d1780090be2fcc4572ef468
Kees Johnson <keesjejohnson@gmail.com>
2023-04-30 10:19:00 +0200

    diagnose + tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,5 +3,9 @@ use toyc;
 
 #[test]
 fn diagnose() {
-    
+    let x = toyc::diagnostics::Diagnostic {
+        location: ("src/main.toy".into(), 1)
+    };
+    toyc::diagnose_push!(x.clone());
+    assert_eq!(toyc::diagnose_pop!(), Some(x))
 }
--- toyc/src/diagnostics.rs
+++ toyc/src/diagnostics.rs
@@ -1,16 +1,30 @@
-pub struct DiagnoseStream(Vec<Diagnostic>);
+pub struct DiagnoseStream(pub Vec<Diagnostic>);
+
 
 pub static mut DIAGNOSTIC_STREAM:DiagnoseStream = DiagnoseStream(Vec::new());
-
-impl core::ops::Shl<Diagnostic> for DiagnoseStream {
-    type Output = DiagnoseStream;
-    fn shl(mut self, rhs: Diagnostic) -> Self::Output {
-        self.0.push(rhs);
-        self
+impl DiagnoseStream {
+    pub fn push(&mut self, v: Diagnostic) {
+        self.0.push(v);
+    }
+    pub fn pop(&mut self) -> Option<Diagnostic> {
+        return self.0.pop();
     }
 }
-
-#[derive(PartialEq, Debug, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(PartialEq, Debug, Eq, PartialOrd, Ord, Clone)]
 pub struct Diagnostic {
-    
+    pub location: (String, usize)
+}
+
+#[macro_export]
+macro_rules! diagnose_push {
+    ($v:expr) => {
+        unsafe {toyc::diagnostics::DIAGNOSTIC_STREAM.push($v)}
+    };
+}
+
+#[macro_export]
+macro_rules! diagnose_pop {
+    () => {
+        unsafe {toyc::diagnostics::DIAGNOSTIC_STREAM.pop()}
+    };
 }
