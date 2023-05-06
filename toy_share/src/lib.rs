25cd02f61d8ff24f1cee1e3df1b77b1e46a1c35e
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2023-05-06 08:13:01 +0200

    bytecode

---
+++ toy_share/src/lib.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+use bincode;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, PartialEq)]
+pub struct Bytecode {
+    pub children: Vec<Command>
+}
+
+impl Bytecode {
+    pub fn ser(&self) -> Result<Vec<u8>, Box<bincode::ErrorKind>> {
+        return bincode::serialize(self);
+    }
+}
+
+pub fn de(v: &Vec<u8>) -> Result<Bytecode, Box<bincode::ErrorKind>> {
+    return bincode::deserialize(v);
+}
+
+#[derive(Serialize, Deserialize, PartialEq)]
+pub enum Command {
+    ImportDyn(String),
+    Return(Bytecode),
+    Get(String),
+    Set(String, Bytecode),
+    Add(Bytecode, Bytecode),
+    Subtract(Bytecode, Bytecode),
+    Negate(Bytecode),
+
+    Bool(bool),
+    Number(i32),
+    String(String),
+    Array(Vec<Bytecode>),
+    Tuple(Vec<Bytecode>),
+    Map(HashMap<String, Bytecode>)
+}
