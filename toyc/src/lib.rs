c24326470fa096070c39a2f12951fc2c820bde35
Kees Johnson <keesjejohnson@gmail.com>
2023-05-09 19:48:42 +0200

    idk

--- toyc/src/lib.rs
+++ toyc/src/lib.rs
@@ -22,23 +22,23 @@ impl SourceCode {
     }
 }
 
-pub trait CcfValue {}
-
-impl CcfValue for bool {}
-impl CcfValue for String {}
-impl CcfValue for toy_share::Bytecode {}
-
 pub struct SkeletonStatusMessage {
+    pub message_type: SkeletonStatusMessageType
+}
 
+pub enum SkeletonStatusMessageType {
+    OpeningFile(String)
 }
 
 pub trait Skeleton {
-    fn import(name: String, name: String) -> SourceCode;
-    fn entry() -> SourceCode;
+    fn import(&self, name: String, display_name: String) -> SourceCode;
+    fn entry(&self) -> SourceCode;
     #[allow(unused_variables)] //This is needed, status should be used if overrided but not normally.
-    fn ccf<T: CcfValue>(key: String) -> T {
-        todo!()
+    fn ccf(&self, key: String) -> toy_share::Bytecode {
+        return toy_share::Bytecode {
+            children: vec![toy_share::Command::Bool(false)] //If no ovveride is given, insert false.
+        };
     }
     #[allow(unused_variables)] //This is needed, status should be used if overrided but not normally.
-    fn report_status(status: SkeletonStatusMessage) {}
+    fn report_status(&self, status: SkeletonStatusMessage) {}
 }
